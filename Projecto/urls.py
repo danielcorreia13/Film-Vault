46a2b0d20c31f44c4981f75b5702bc0b98eeb2b3
José Silva <ze@ze.lan>
2020-11-11 18:32:51 +0000

    Base searchpage

--- Projecto/urls.py
+++ Projecto/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from FilmVault import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('search/', views.searchpage, name='searchpage'),
 ]
