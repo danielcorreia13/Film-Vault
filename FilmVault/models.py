357aabc811b11a8d67e218759e3e2c71d64193cc
Daniel <daniel-correia@live.com.pt>
2020-11-11 20:26:29 +0000

    movies by year

--- FilmVault/models.py
+++ FilmVault/models.py
@@ -1,3 +1,31 @@
+import xmltodict
+from BaseXClient import BaseXClient
 from django.db import models
+from enum import Enum
 
-# Create your models here.
+class UDF():
+    filmsByYear = "funcs:filmsOrderByYearPage({}, {} {})"
+
+module_import = "import module namespace funcs = 'funcs' at '/Data/Universidade/EDC/pythonProject/Querys/funcs/funcs.xq'; \n "
+query = "xquery {} {}"
+
+
+
+def getFilmsSortedByYear(pageIndex, n, syear = None, fyear = None, cat = None):
+    session = BaseXClient.Session('localhost', 1984, 'admin', 'admin')
+
+    result = None
+    if cat:
+        if syear != None and fyear != None:
+            result = session.execute(query.format(module_import, UDF.filmsByYear.format(pageIndex, n, ", {}, {}, {}".format(syear, fyear, cat))))
+        else:
+            result = session.execute(query.format(module_import, UDF.filmsByYear.format(pageIndex, n, ", {}".format(cat))))
+    else:
+        if syear != None and fyear != None:
+            result = session.execute(query.format(module_import, UDF.filmsByYear.format(pageIndex, n, ", {}, {}".format(syear, fyear))))
+        else:
+            result = session.execute(query.format(module_import, UDF.filmsByYear.format(pageIndex, n, '')))
+
+    dict = xmltodict.parse(result)
+
+    return [movie for movie in dict['root']['elem']]
